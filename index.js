3e7c305a0099fa84e9e55f9a3b786a6489af4519
Stylesdesign <elijahfavour72@gmail.com>
2026-03-29 13:44:25 -0700

    Upgraded CLI app with create, view, edit, delete features

--- index.js
+++ index.js
@@ -1,37 +1,113 @@
 const fs = require("fs");
-
-const command = process.argv[2];
-const title = process.argv[3];
-const content = process.argv[4];
+const readline = require("readline");
 
 const file = "posts.json";
 
-// Load existing posts
+// Load posts
 let posts = [];
-
 if (fs.existsSync(file)) {
-  const data = fs.readFileSync(file);
-  posts = JSON.parse(data);
+  posts = JSON.parse(fs.readFileSync(file));
 }
 
-// Add post
-if (command === "add") {
-  const newPost = {
-    title: title,
-    content: content,
-  };
+// Setup CLI input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  posts.push(newPost);
+function showMenu() {
+  console.log("\n=== Blog CLI ===");
+  console.log("1. Create Post");
+  console.log("2. View Posts");
+  console.log("3. Edit Post");
+  console.log("4. Delete Post");
+  console.log("5. Exit");
 
+  rl.question("Choose an option: ", handleMenu);
+}
+
+function handleMenu(choice) {
+  switch (choice) {
+    case "1":
+      createPost();
+      break;
+    case "2":
+      viewPosts();
+      break;
+    case "3":
+      editPost();
+      break;
+    case "4":
+      deletePost();
+      break;
+    case "5":
+      rl.close();
+      break;
+    default:
+      console.log("Invalid choice");
+      showMenu();
+  }
+}
+
+function savePosts() {
   fs.writeFileSync(file, JSON.stringify(posts, null, 2));
+}
 
-  console.log("Post added!");
-} else if (command === "list") {
-  console.log("All Posts:");
+function createPost() {
+  rl.question("Enter title: ", (title) => {
+    rl.question("Enter content: ", (content) => {
+      posts.push({ title, content });
+      savePosts();
+      console.log("Post created!");
+      showMenu();
+    });
+  });
+}
 
+function viewPosts() {
+  console.log("\nAll Posts:");
   posts.forEach((post, index) => {
     console.log(`${index + 1}. ${post.title} - ${post.content}`);
   });
-} else {
-  console.log("Unknown command");
+  showMenu();
 }
+
+function editPost() {
+  viewPosts();
+  rl.question("Enter post number to edit: ", (num) => {
+    const index = num - 1;
+
+    if (posts[index]) {
+      rl.question("New title: ", (title) => {
+        rl.question("New content: ", (content) => {
+          posts[index] = { title, content };
+          savePosts();
+          console.log("Post updated!");
+          showMenu();
+        });
+      });
+    } else {
+      console.log("Invalid post");
+      showMenu();
+    }
+  });
+}
+
+function deletePost() {
+  viewPosts();
+  rl.question("Enter post number to delete: ", (num) => {
+    const index = num - 1;
+
+    if (posts[index]) {
+      posts.splice(index, 1);
+      savePosts();
+      console.log("Post deleted!");
+    } else {
+      console.log("Invalid post");
+    }
+    showMenu();
+  });
+}
+
+// Start app
+showMenu();
